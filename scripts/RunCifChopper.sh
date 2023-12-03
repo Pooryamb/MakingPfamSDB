a1f58263e56e0808665c1307d094ba780e8f1ae2
Pooryamb <90681461+Pooryamb@users.noreply.github.com>
2023-12-03 17:01:55 -0500

    Update RunCifChopper.sh
    Sometimes, the tar file made by python is not parsable by Foldseek. The updated script extracts the Tar file and retars the PDB files using Tar instead of python.

--- scripts/RunCifChopper.sh
+++ scripts/RunCifChopper.sh
@@ -12,3 +12,14 @@ set -e
 batch=B$SLURM_ARRAY_TASK_ID
 
 python CifChopperHandler.py ../structures/${batch}.tar ./tmp/${batch}_Cords.txt
+
+## Sometimes, the tar file made by python is not parsable by Foldseek. The following parts extract the Tar file and retar the PDB files using Tar instead of python
+mkdir ../structures/${batch}
+cd ../structures/${batch}
+mv ../${batch}_chopped.tar .
+tar -xf ${batch}_chopped.tar
+tar -cf ../${batch}_chopped.tar *.pdb.gz
+rm *.pdb.gz
+cd ..
+rm -r ${batch}
+cd ../scripts
